418c772537e82d09163f42869bd2460ce06f6eb3
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-10-18 21:48:08 -0300

    model classes

---
+++ StocksReport/Model/Portfolio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace StocksReport.Model
+{
+    public class Portfolio
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public double LastPrice { get; set; }
+        public double Change { get; set; }
+        public int Shares { get; set; }
+        public double Price { get; set; }
+        public double Cost { get; set; }
+        public double Value { get; set; }
+        public double Gain { get; set; }
+        public List<SymbolPriceHistory> PriceHistory { get; set; }
+    }
+
+}
---
+++ StocksReport/Model/SymbolPriceHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksReport.Model
+{
+
+    public class SymbolPriceHistory
+    {
+        public DateTime PriceDate { get; set; }
+        public string PriceMonth { get; set; }
+        public string Symbol { get; set; }
+        public double Price { get; set; }
+        public double PriceGrowth { get; set; }
+    }
+
+}
