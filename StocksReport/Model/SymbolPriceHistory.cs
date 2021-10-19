34b80aec9f20ed18b5c5cbf8f02e0eb7ff6911ee
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-10-19 11:33:46 -0300

    template - Stocks Portfolios

--- StocksReport/Model/SymbolPriceHistory.cs
+++ StocksReport/Model/SymbolPriceHistory.cs
@@ -9,7 +9,7 @@ namespace StocksReport.Model
     public class SymbolPriceHistory
     {
         public DateTime PriceDate { get; set; }
-        public string PriceMonth { get; set; }
+        public string PriceMonth { get { return PriceDate.ToString("MMM-yyyy"); } }
         public string Symbol { get; set; }
         public double Price { get; set; }
         public double PriceGrowth { get; set; }
